818306f6ce0df12738061f7558c2257e1c7a39e5
Hustle <95133306+hzr31479@users.noreply.github.com>
2022-08-09 00:49:15 +0800

    [feature]完成第六章: 敌人，第1部分：静态Observer
    通过C#脚本,熟悉射线,碰撞体等unity接口

--- 3D_Test/Assets/Scripts/GameEnding.cs
+++ 3D_Test/Assets/Scripts/GameEnding.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
     public GameObject player;
-    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup exitBackgroundImageCanvasGroup; 
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
-
+    
     void OnTriggerEnter (Collider other)
     {
         if (other.gameObject == player)
@@ -20,23 +23,38 @@ public class GameEnding : MonoBehaviour
         }
     }
 
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
+
     void Update ()
     {
-        if(m_IsPlayerAtExit)
+        if (m_IsPlayerAtExit)
         {
-            EndLevel ();
+            EndLevel (exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel ()
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
-
-        if(m_Timer > fadeDuration + displayImageDuration)
+        if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit ();
+            if (doRestart)
+            {
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+                Application.Quit ();
+            }
         }
     }
 }
