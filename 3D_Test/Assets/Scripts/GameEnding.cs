d1a8bbbfd373e5d5d3a003d05ae41ea335834bfc
Hustle <95133306+hzr31479@users.noreply.github.com>
2022-08-09 23:04:08 +0800

    [feature]完成第八第九章:动态敌人制作;音频配置
    1.学习动态敌人制作,自动路径学习,学习如何应用音频效果

--- 3D_Test/Assets/Scripts/GameEnding.cs
+++ 3D_Test/Assets/Scripts/GameEnding.cs
@@ -1,60 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class GameEnding : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float fadeDuration = 1f;
-    public float displayImageDuration = 1f;
-    public GameObject player;
-    public CanvasGroup exitBackgroundImageCanvasGroup; 
-    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public float turnSpeed = 20f;
 
-    bool m_IsPlayerAtExit;
-    bool m_IsPlayerCaught;
-    float m_Timer;
-    
-    void OnTriggerEnter (Collider other)
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start ()
     {
-        if (other.gameObject == player)
-        {
-            m_IsPlayerAtExit = true;
-        }
+        m_Animator = GetComponent<Animator> ();
+        m_Rigidbody = GetComponent<Rigidbody> ();
+        m_AudioSource = GetComponent<AudioSource> ();
     }
 
-    public void CaughtPlayer ()
+    void FixedUpdate ()
     {
-        m_IsPlayerCaught = true;
-    }
+        float horizontal = Input.GetAxis ("Horizontal");
+        float vertical = Input.GetAxis ("Vertical");
+        
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
 
-    void Update ()
-    {
-        if (m_IsPlayerAtExit)
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool ("IsWalking", isWalking);
+        
+        if (isWalking)
         {
-            EndLevel (exitBackgroundImageCanvasGroup, false);
-        }
-        else if (m_IsPlayerCaught)
-        {
-            EndLevel (caughtBackgroundImageCanvasGroup, true);
-        }
-    }
-
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
-    {
-        m_Timer += Time.deltaTime;
-        imageCanvasGroup.alpha = m_Timer / fadeDuration;
-
-        if (m_Timer > fadeDuration + displayImageDuration)
-        {
-            if (doRestart)
+            if (!m_AudioSource.isPlaying)
             {
-                SceneManager.LoadScene (0);
-            }
-            else
-            {
-                Application.Quit ();
+                m_AudioSource.Play();
             }
         }
+        else
+        {
+            m_AudioSource.Stop ();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation (desiredForward);
+    }
+
+    void OnAnimatorMove ()
+    {
+        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation (m_Rotation);
     }
 }
